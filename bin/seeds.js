97355d741eff74edb780be6403f58c7b62661a4c
Louis MD <louis@margot-duclot.fr>
2020-02-06 13:58:06 +0100

    More shoes seeded

--- bin/seeds.js
+++ bin/seeds.js
@@ -3,21 +3,48 @@ require ("./../config/mongodb.js");
 const Sneaker = require("./../models/Sneaker");
 const Tag = require("./../models/Tag");
 
-// const sneakers = [
-//   {
-//     name: 'Merrel',
-//     ref: '12SDFE3',
-//     sizes: [38,39,40,41,42],
-//     description: 'Great hiking shoes',
-//     image: 'https://cdn.pixabay.com/photo/2014/12/31/11/41/shoes-584850__480.jpg',
-//     price: 122,
-//     category: "men"
-//   }
-// ];
-//
-// Sneaker.insertMany(sneakers)
-//   .then(response => console.log("SEEDED DB WITH SNEAKERS !"))
-//   .catch(error => console.log("ERROR : ",error))
+const sneakers = [
+  {
+    name: 'Merrel',
+    ref: '12SDFE3',
+    sizes: [38,39,40,41,42],
+    description: 'Great hiking shoes',
+    image: 'https://cdn.pixabay.com/photo/2014/12/31/11/41/shoes-584850__480.jpg',
+    price: 122,
+    category: "men"
+  }, 
+  {
+    name: 'AirMax',
+    ref: '354KJ4L53LJ4',
+    sizes: [38,39,40,41,42,43,46,52],
+    description: 'The greatest sneakers of all time',
+    image: 'https://www.dhresource.com/600x600/f2/albu/g9/M00/5A/A3/rBVaVV0YzE2ABXHQAAIEq2Mja8k398.jpg',
+    price: 155,
+    category: "women"
+  },
+  {
+    name: 'Scholl',
+    ref: 'SLDFKJ3453',
+    sizes: [12,13,4],
+    description: "Brice de Nice's favorite shoes. Not sneakers",
+    image: 'http://t0.gstatic.com/images?q=tbn%3AANd9GcRxbcEYcJ64Oe2swW2zk2_ACwC4nzcdB1CYbiz_B-BlVIqywRpHSztMjr3rqOmkdhMJwyY8Z0U&usqp=CAc',
+    price: 15,
+    category: "kids"
+  },
+  {
+    name: 'Bozo',
+    ref: 'LOLILOL',
+    sizes: [122],
+    description: 'On rigole bien',
+    image: 'http://t0.gstatic.com/images?q=tbn%3AANd9GcQBeJY-9tV5V8lN0I_XdmRf0McVQPYmLMCNRDAteadOCZWw2tylBCCejDGHFNC5kyUjEAW9cP7U&usqp=CAc',
+    price: 1,
+    category: "men"
+  }
+];
+
+Sneaker.insertMany(sneakers)
+  .then(response => console.log("SEEDED DB WITH SNEAKERS !"))
+  .catch(error => console.log("ERROR : ",error))
 
 
 const tags = [
