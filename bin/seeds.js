2acc7899d86c8cda9f2f208fa9ef6994eb87cb96
Alex Meyer <alexandre.meyer78@gmail.com>
2020-02-06 12:37:03 +0100

    One product page

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,19 +1,36 @@
 require("dotenv").config();
 require ("./../config/mongodb.js");
 const Sneaker = require("./../models/Sneaker");
+const Tag = require("./../models/Tag");
 
-const sneakers = [
-  {
-    name: 'Merrel',
-    ref: '12SDFE3',
-    sizes: [38,39,40,41,42],
-    description: 'Great hiking shoes',
-    image: 'https://cdn.pixabay.com/photo/2014/12/31/11/41/shoes-584850__480.jpg',
-    price: 122,
-    category: "men"
-  }
+// const sneakers = [
+//   {
+//     name: 'Merrel',
+//     ref: '12SDFE3',
+//     sizes: [38,39,40,41,42],
+//     description: 'Great hiking shoes',
+//     image: 'https://cdn.pixabay.com/photo/2014/12/31/11/41/shoes-584850__480.jpg',
+//     price: 122,
+//     category: "men"
+//   }
+// ];
+//
+// Sneaker.insertMany(sneakers)
+//   .then(response => console.log("SEEDED DB WITH SNEAKERS !"))
+//   .catch(error => console.log("ERROR : ",error))
+
+
+const tags = [
+  {label : "hiking"},
+  {label : "running"},
+  {label : "urban"},
+  {label : "orthopedic"},
+  {label : "ugly"},
+  {label : "clown"},
+  {label : "skateboarding"},
+  {label : "trashy"}
 ];
 
-Sneaker.insertMany(sneakers)
-  .then(response => console.log("SEEDED DB !"))
+Tag.insertMany(tags)
+  .then(response => console.log("SEEDED DB WITH TAGS !"))
   .catch(error => console.log("ERROR : ",error))
