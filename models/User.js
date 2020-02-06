269b64c2c66a586d8bdeb639599ca46cba0fa1a7
Alex Meyer <alexandre.meyer78@gmail.com>
2020-02-06 15:57:58 +0100

    Signup method

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-  name: {type: String, required:true},
+  firstname: {type: String, required:true},
   lastname: {type: String, required:true},
   email: {type: String, required:true, unique:true},
   password: {type: String, required:true}
--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,40 @@
 const express = require("express");
 const router  = new express.Router();
 const bcrypt  = require("bcryptjs");
+const User = require("./../models/User");
 
 router.get("/signup", (req, res) => {
   res.render("signup");
-  // res.send("sneak");
-  // req.session.msg
+
+});
+
+router.post("/signup", (req, res, next) => {
+  const newUser = req.body;
+  User
+    .findOne({ email: newUser.email })
+    .then(dbRes => {
+
+      if (dbRes) {
+        // TODO FLASH MESSAGES
+        // req.flash("error", "sorry, email is already taken :/");
+        return res.redirect("/auth/signup");
+      }
+
+      newUser.password = bcrypt.hashSync(newUser.password, bcrypt.genSaltSync(10))
+
+      User.create(newUser, function(err, data){
+        if (err) throw new Error('Error creating user');
+
+        req.session.currentUser = data;
+        res.redirect("/manager/all");
+      })
+
+    }).catch(next)
+
 });
 
 router.get("/signin", (req, res) => {
-  res.send("love");
+  res.render("signin");
 });
 
 module.exports = router;
